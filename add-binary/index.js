974acfb33fa57a0f343ab73a3a0ce15394b9e830
Jimmy Zhao <itomgit@gmail.com>
2016-05-06 14:38:02 +0800

    Submission of problem add binary

---
+++ add-binary/index.js
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/add-binary/
+ */
+var addBinary = function(a, b) {
+    var result = "";
+    var len = a.length > b.length ? a.length : b.length;
+    if (diff) {
+      for (i = len - diff - 1; i <= len - 1; i++) {
+        if (!a[i]) {
+          a = "0" + a;
+        }
+        if (!b[i]) {
+          b = "0" + b;
+        }
+      }
+    }
+    var cache = 0;
+    for (i = len - 1; i >= 0; i--) {
+      var sum = (parseInt(a[i]) + parseInt(b[i]) + cache);
+      if (sum >= 2) {
+        cache = 1;
+        sum = sum === 3 ? 1 : 0;
+      } else {
+        cache = 0;
+      }
+      result = sum.toString() + result;
+    }
+    if (cache === 1) {
+      result = cache.toString() + result;
+    }
+    return result;
+};
